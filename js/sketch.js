786c594f3d96ebd4efc5d9711684e81dc0670143
William Lind <william.lind2@griffithuni.edu.au>
2023-08-28 12:10:11 +1000

    git commit

--- js/sketch.js
+++ js/sketch.js
@@ -1,9 +1,16 @@
 "use strict";
-
+let angle=0;
+let speed=0.05;
+let scalar=100;
 function setup(){
     createCanvas(400,400);
 }
 
 function draw(){
     background(120,120,120);
+    let x=width/2+sin(angle)*scalar;
+    let y=height/2+cos(angle)*scalar;
+    ellipse(x,y,25,25);
+    angle+=speed;
+
 }
